5a43568188327ab8cd51f5d96d9a1939b2f0d96c
MonsterPi13 <rpishappynow@gmail.com>
2024-04-26 18:57:51 +0800

    fix: the loader in homepage style error

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { Html } from "@react-three/drei";
 
 const Loader = () => {
   return (
-    <Html>
+    <Html className="!-translate-x-1/2 !-translate-y-1/2">
       <div className="border border-blue-400 border-t-transparent w-16 h-16 rounded-full animate-spin"></div>
     </Html>
   );
